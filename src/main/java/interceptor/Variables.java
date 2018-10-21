5cf681ec907cc92e44d12cc1bb6ad966311f8fdd
Pedro Manuel Gómez-Portiollo <gomezportillo@correo.ugr.es>
2018-10-21 14:24:51 +0200

    added class to store common variables

---
+++ src/main/java/interceptor/Variables.java
@@ -0,0 +1,17 @@
+package interceptor;
+
+public class Variables 
+{
+	public static final String TXT_ENGINE_ON = "ENGINE ON";
+	public static final String TXT_ENGINE_OFF = "ENGINE OFF";
+	
+	public static final String TXT_TURN_ON = "TURN ON";
+	public static final String TXT_TURN_OFF = "TURN OFF";
+	
+	public static final String TXT_ACCELERATE = "ACCELERATE";
+	
+	public static final boolean STATE_ENGINE_ON = true;
+	public static final boolean STATE_ENGINE_OFF = false;
+
+};
+
--- src/main/java/interceptor/WebController.java
+++ src/main/java/interceptor/WebController.java
@@ -1,82 +1,97 @@
 package interceptor;
 
-import javax.faces.event.ActionEvent;
 import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
+import javax.faces.event.ActionEvent;
 
 @ManagedBean(name = "webController", eager = true)
 @SessionScoped
 public class WebController implements Serializable
 {
-	private static final long serialVersionUID = 1L;
 
-    @ManagedProperty( value = "#{state}" )
-    private String state;
-    
-    @ManagedProperty( value = "#{textOnOff}" )
-    private String textOnOff;
-    
-    @ManagedProperty( value = "#{textAccelerate}" )
-    private String textAccelerate;
-    
+	private static final long serialVersionUID = -398064717122939161L;
+
+	@ManagedProperty( value = "#{textState}" )
+	private String textState;
+
+	@ManagedProperty( value = "#{textOnOff}" )
+	private String textOnOff;
+
+	@ManagedProperty( value = "#{textAccelerate}" )
+	private String textAccelerate;
+
+	private boolean state = Variables.STATE_ENGINE_OFF;
+
 	public WebController()
-	{
+	{ 
+		System.out.println("Creating web controller");
 	}
 
-    public void toggleOnOff( ActionEvent e )
-    {
-    	System.out.println("Turning on/off...");
-    }
-	
-    public void accelerate( ActionEvent e )
-    {
-    	System.out.println("Accelerating...");
-    }
-	
-	public String getState() 
+	public void toggleOnOff( ActionEvent e )
 	{
-		if ( this.state == null )
+		if ( this.state == Variables.STATE_ENGINE_ON )
 		{
-			this.state = "ENGINE OFF";
+			System.out.println("Turning off...");
+			this.textState = Variables.TXT_ENGINE_OFF;
+			this.textOnOff = Variables.TXT_TURN_ON;
 		}
-		return this.state;
+		else
+		{
+			System.out.println("Turning on...");
+			this.textState = Variables.TXT_ENGINE_ON;
+			this.textOnOff = Variables.TXT_TURN_OFF;
+		}
+		this.state = !this.state;
 	}
 
-	public void setState( String s ) 
+	public void accelerate( ActionEvent e )
 	{
-		
-		this.state = s;
+		System.out.println("Accelerating...");
 	}
-    
+
+	public String getTextState() 
+	{
+		if ( this.textState == null )
+		{
+			this.textState = Variables.TXT_ENGINE_OFF;
+		}
+		return this.textState;
+	}
+
+	public void setTextState( String s ) 
+	{
+		this.textState = s;
+	}
+
 	public String getTextOnOff()
 	{
 		if ( this.textOnOff == null)
 		{
-			this.textOnOff = "TURN ON";
+			this.textOnOff = Variables.TXT_TURN_ON;
 		}
 		return this.textOnOff;
 	}
-	
+
 	public void setTextOnOff( String t )
 	{
 		this.textOnOff = t;
 	}
-	
+
 	public String getTextAccelerate()
 	{
 		if ( this.textAccelerate == null)
 		{
-			this.textAccelerate = "ACCELERATE";
+			this.textAccelerate = Variables.TXT_ACCELERATE;
 		}
 		return this.textAccelerate;
 	}
-	
-	public void setTextAccelerate( String a )
+
+	public void setTextAccelerate( String t )
 	{
-		this.textAccelerate = a;
+		this.textAccelerate = t;
 	}
-    
-	
+
+
 }
